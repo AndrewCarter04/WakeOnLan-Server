4cbd01ea4ee36d11f0ab8441d3b26f34aa21b6b6
James <james@jamesmcc.co.uk>
2024-03-24 15:43:27 +0000

    Made both buttons Asynchronous

--- static/main.js
+++ static/main.js
@@ -1,34 +1,49 @@
-function raw_trigger(url, id) {
-    console.log(id)
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', url, false);
-    xhr.setRequestHeader("Content-Type", "application/json")
-    xhr.send(JSON.stringify({"id": id}));
-    return xhr
-
-}
 
 function trigger_wakeup() {
-    let result = raw_trigger("/wake", this.dataset.id)
-    if (result.status === 200) {
-        alert("Success!")
-    } else {
-        alert(result.responseText)
+    let id = this.dataset.id
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', "/wake", true);
+    xhr.onload = e => {
+        if (xhr.readyState === 4) { // Once Request is Complete
+            if (xhr.status === 200) {
+                alert("Success!")
+            } else {
+                alert(xhr.responseText)
+            }
+        }
     }
+    xhr.onerror = e => {
+        console.log(xhr.responseText)
+    }
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send(JSON.stringify({"id": id}));
 }
 
 function ping_test() {
-    let result = raw_trigger("/ping", this.dataset.id)
-    if (result.status === 200) {
-        let resObj = JSON.parse(result.responseText)
-        if (resObj.status === "online") {
-            alert("Device Online")
-        } else if (resObj.status === "offline") {
-            alert("Device Offline")
-        } else {
-            alert("An Error Occured: " + resObj)
+    let id = this.dataset.id
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', "/ping", true);
+    xhr.onload = e => {
+        if (xhr.readyState === 4) { // Once Request is Complete
+            if (xhr.status === 200) {
+                let resObj = JSON.parse(xhr.responseText)
+                if (resObj.status === "online") {
+                    alert("Device Online")
+                } else if (resObj.status === "offline") {
+                    alert("Device Offline")
+                } else {
+                    alert("An Error Occured: " + resObj)
+                }
+            } else {
+                alert(xhr.responseText)
+            }
         }
     }
+    xhr.onerror = e => {
+        console.log(xhr.responseText)
+    }
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send(JSON.stringify({"id": id}));
 }
 
 window.addEventListener("load", () => {
